0f934e2008474e8f1aa8ff8898ff7aba493f06c8
goldipl <mgodlewski92@gmail.com>
2021-02-25 18:44:28 +0100

    source path add

--- js/scriptText.js
+++ js/scriptText.js
@@ -1,22 +1,26 @@
+// StackOverflow Source path: 
+// https://stackoverflow.com/questions/62678282/add-delay-when-index-increase/62678368#62678368
+
 const texts = ['Jestem Marcin.',
-              'Jestem Web Developerem',
-              'oraz Junior Java Developerem.',
-              'Jestem programistą.',
-              'Jestem nastawiony na sukces.'];
+    'Jestem Web Developerem',
+    'oraz Junior Java Developerem.',
+    'Jestem programistą.',
+    'Jestem nastawiony na sukces.'
+];
 let count = 0;
 let index = 0;
 let currentText = "";
 let letter = "";
 
-(function type(){
-    if(count === texts.length){
+(function type() {
+    if (count === texts.length) {
         count = 0;
     }
     currentText = texts[count];
     letter = currentText.slice(0, ++index);
-    
+
     document.querySelector(".typing").textContent = letter;
-    if(letter.length === currentText.length){
+    if (letter.length === currentText.length) {
         count++;
         index = 0;
     }
