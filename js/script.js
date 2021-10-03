61c5ee9c0941d5ac529a46f64c0a7c367f5798d1
goldipl <mgodlewski92@gmail.com>
2021-10-03 12:29:11 +0200

    add blocked body css & js

--- js/script.js
+++ js/script.js
@@ -3,10 +3,13 @@ const burgerMenu = document.querySelector(".burgerMenu");
 const iconBurgerMenu = document.querySelector(".fa-bars");
 const iconClose = document.querySelector(".fa-times");
 const column = document.querySelector("aside");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
 
 burgerMenu.addEventListener("click", function() {
     iconBurgerMenu.classList.toggle("show");
     iconClose.classList.toggle("show");
     column.classList.toggle("show");
-
-})
+    body.classList.toggle("blockedBody");
+    header.classList.toggle("headerOpacity");
+});
