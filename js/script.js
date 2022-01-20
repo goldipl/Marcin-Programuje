ed108f1798311cadfeaad96d7daff6f2a12ea427
goldipl <mgodlewski92@gmail.com>
2022-01-20 15:59:17 +0100

    ad new script to handle totally new navigation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
-const burgerMenu = document.querySelector(".burgerMenu");
+const burger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobileMenu");
+const mobileMenuLi = document.querySelector(".mobileMenu_li");
 
-const iconBurgerMenu = document.querySelector(".fa-bars");
-const iconClose = document.querySelector(".fa-times");
-const column = document.querySelector("aside");
-const body = document.querySelector("body");
-const header = document.querySelector("header");
-
-burgerMenu.addEventListener("click", function() {
-    iconBurgerMenu.classList.toggle("show");
-    iconClose.classList.toggle("show");
-    column.classList.toggle("show");
-    body.classList.toggle("blockedBody");
-    header.classList.toggle("headerOpacity");
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
 });
+
+mobileMenuLi.addEventListener("click", () => {
+    burger.classList.remove("active");
+    mobileMenu.classList.remove("active");
+});
+
 
 /* footer year */
 document.querySelector("span.footerYear").innerHTML = new Date().getFullYear();
